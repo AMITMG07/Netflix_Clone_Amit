91ee984082a4079fd36013ec26eb2bf05b235524
AMITMG07 <56391460+AMITMG07@users.noreply.github.com>
2025-06-17 10:55:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <WishlistProvider>
-        <HashRouter>
+       
           <App />
-        </HashRouter>
+        
         
       </WishlistProvider>
     </BrowserRouter>
